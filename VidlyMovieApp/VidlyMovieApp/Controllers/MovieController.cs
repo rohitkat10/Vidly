d87b5c353c38d3d86575bb3e087d8409519ab05c
rohitkat10 <rohitkat10@gmail.com>
2017-03-14 16:51:50 +0530

    Create Movie Module

---
+++ VidlyMovieApp/VidlyMovieApp/Controllers/MovieController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VidlyMovieApp.Models;
+
+namespace VidlyMovieApp.Controllers
+{
+    public class MovieController : Controller
+    {
+        //
+        // GET: /Movie/
+        public ActionResult Random()
+        {
+            var Movie = new Movie { Name = "RockStar" };
+            return View(Movie);
+        }
+	}
+}
